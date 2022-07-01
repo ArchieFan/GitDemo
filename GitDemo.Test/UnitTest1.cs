27b4134c772388e0229e59717e0f0bcf7dbebd8b
Archie Fan <archiefan@gmail.com>
2022-07-01 11:39:10 +0100

    init changed

---
+++ GitDemo.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace GitDemo.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void XUnitTest1()
+        {
+            Program pro = new Program();
+
+        }
+    }
+}
